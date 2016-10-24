d05871e5d64c9c648c0cab6124278a081f6e51bc
August Valera <4U6U57@gmail.com>
2016-10-24 17:14:49 +0000

    Added Add product link, change logo generation

--- models/menu.py
+++ models/menu.py
@@ -5,8 +5,9 @@
 # Customize your APP title, subtitle and menus here
 # ----------------------------------------------------------------------------------------------------------------------
 
-response.logo = A(B('web', SPAN(2), 'py'), XML('&trade;&nbsp;'),
-                  _class="navbar-brand", _href="http://www.web2py.com/",
+response.logo = A(B('Storefront ',
+                  I(_class="fa fa-shopping-cart")),
+                  _class="navbar-brand", _href=URL("default", "index"),
                   _id="web2py-logo")
 response.title = request.application.replace('_', ' ').title()
 response.subtitle = ''
@@ -49,8 +50,9 @@ def _():
     # useful links to internal and external resources
     # ------------------------------------------------------------------------------------------------------------------
     response.menu += [
-        (T('My Sites'), False, URL('admin', 'default', 'site')),
-        (T('This App'), False, '#', [
+        (T('Developer'), False, '#', [
+            (T('Admin'), False, URL('admin', 'default', 'site')),
+            LI(_class="divider"),
             (T('Design'), False, URL('admin', 'default', 'design/%s' % app)),
             LI(_class="divider"),
             (T('Controller'), False,
@@ -80,67 +82,7 @@ def _():
             (T('About'), False, URL(
                 'admin', 'default', 'about/' + app)),
         ]),
-        ('web2py.com', False, '#', [
-            (T('Download'), False,
-             'http://www.web2py.com/examples/default/download'),
-            (T('Support'), False,
-             'http://www.web2py.com/examples/default/support'),
-            (T('Demo'), False, 'http://web2py.com/demo_admin'),
-            (T('Quick Examples'), False,
-             'http://web2py.com/examples/default/examples'),
-            (T('FAQ'), False, 'http://web2py.com/AlterEgo'),
-            (T('Videos'), False,
-             'http://www.web2py.com/examples/default/videos/'),
-            (T('Free Applications'),
-             False, 'http://web2py.com/appliances'),
-            (T('Plugins'), False, 'http://web2py.com/plugins'),
-            (T('Recipes'), False, 'http://web2pyslices.com/'),
-        ]),
-        (T('Documentation'), False, '#', [
-            (T('Online book'), False, 'http://www.web2py.com/book'),
-            LI(_class="divider"),
-            (T('Preface'), False,
-             'http://www.web2py.com/book/default/chapter/00'),
-            (T('Introduction'), False,
-             'http://www.web2py.com/book/default/chapter/01'),
-            (T('Python'), False,
-             'http://www.web2py.com/book/default/chapter/02'),
-            (T('Overview'), False,
-             'http://www.web2py.com/book/default/chapter/03'),
-            (T('The Core'), False,
-             'http://www.web2py.com/book/default/chapter/04'),
-            (T('The Views'), False,
-             'http://www.web2py.com/book/default/chapter/05'),
-            (T('Database'), False,
-             'http://www.web2py.com/book/default/chapter/06'),
-            (T('Forms and Validators'), False,
-             'http://www.web2py.com/book/default/chapter/07'),
-            (T('Email and SMS'), False,
-             'http://www.web2py.com/book/default/chapter/08'),
-            (T('Access Control'), False,
-             'http://www.web2py.com/book/default/chapter/09'),
-            (T('Services'), False,
-             'http://www.web2py.com/book/default/chapter/10'),
-            (T('Ajax Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/11'),
-            (T('Components and Plugins'), False,
-             'http://www.web2py.com/book/default/chapter/12'),
-            (T('Deployment Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/13'),
-            (T('Other Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/14'),
-            (T('Helping web2py'), False,
-             'http://www.web2py.com/book/default/chapter/15'),
-            (T("Buy web2py's book"), False,
-             'http://stores.lulu.com/web2py'),
-        ]),
-        (T('Community'), False, None, [
-            (T('Groups'), False,
-             'http://www.web2py.com/examples/default/usergroups'),
-            (T('Twitter'), False, 'http://twitter.com/web2py'),
-            (T('Live Chat'), False,
-             'http://webchat.freenode.net/?channels=web2py'),
-        ]),
+        (T('Add Product'), False, URL('default', 'product'))
     ]
 
 
